66b2d2a22f9e30cc938722279dde4d2f71c97705
Chiara Basile <basilechiara@libero.it>
2024-06-23 15:21:01 +0200

    switch to combineV10

--- bdt_cut_optimization/binBDT_LxyS1.5_HLT_overlap_2024May27_bkgModel/to_run_fit_and_limits.sh
+++ bdt_cut_optimization/binBDT_LxyS1.5_HLT_overlap_2024May27_bkgModel/to_run_fit_and_limits.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 #cd ${COMBINE}/WTau3Mu_limits/bdt_cut_optimization/
-BASE_DIR='/eos/user/c/cbasile/CombineTools/CMSSW_11_3_4/src/WTau3Mu_limits/'
+BASE_DIR="$CMSSW_BASE/src/WTau3Mu_limits/"
 COMBINE_DIR="$BASE_DIR/bdt_cut_optimization/binBDT_LxyS1.5_HLT_overlap_2024May27_bkgModel/input_combine"
 EOS_DIR='/eos/user/c/cbasile/www/Tau3Mu_Run3/BDTtraining/cut_LxySign/Training_kFold_HLT_overlap_LxyS1.5_2024May27/bdt_scan_bkgModel/'
 YEAR=22
@@ -10,21 +10,21 @@ DATA='/eos/user/c/cbasile/Tau3MuRun3/data/mva_data/XGBout_data_kFold_HLT_overlap
 
 # 2022
 CATEGORY_LIST=("A" "B" "C")
-CATEGORY_LIST=("C")
+CATEGORY_LIST=("A")
 for CATEGORY in ${CATEGORY_LIST[@]}; do
     echo -e "\n----------------"
     echo    "|  CATEGORY $CATEGORY  |"
     echo -e "----------------\n"
     echo 'TIME TO FIT'
-    #python3 $BASE_DIR/models/Tau3Mu_fitSB.py --plot_outdir $EOS_DIR --combine_dir $COMBINE_DIR -s $SIGNAL -d $DATA --category $CATEGORY -y $YEAR --tag $TAG --optim_bdt --save_ws --const_lowB
+    python3 $BASE_DIR/models/Tau3Mu_fitSB.py --plot_outdir $EOS_DIR --combine_dir $COMBINE_DIR -s $SIGNAL -d $DATA --category $CATEGORY -y $YEAR --tag $TAG --optim_bdt --save_ws -b dynamic --BDTmin 0.9900 --BDTmax 0.9910 --BDTstep 0.0005
     echo 'TIME TO CALCULATE LIMITS'
     # with AsymptoticLimits
-    #python3 $BASE_DIR/bdt_cut_optimization/runBDTOptimCombine.py -i $COMBINE_DIR -o $EOS_DIR --scan_sensitivity input_combine/sensitivity_tree_bdt_scan_WTau3Mu_$CATEGORY${YEAR}_$TAG.root -d WTau3Mu_$CATEGORY$YEAR -n WTau3Mu_$CATEGORY$YEAR -s all 
+    python3 $BASE_DIR/models/runBDTOptimCombine.py -i $COMBINE_DIR -o $EOS_DIR --scan_sensitivity input_combine/sensitivity_tree_bdt_scan_WTau3Mu_$CATEGORY${YEAR}_$TAG.root -d WTau3Mu_$CATEGORY$YEAR -n WTau3Mu_$CATEGORY$YEAR --BDTmin 0.9900 --BDTmax 0.9910 --BDTstep 0.0005 -s all 
     # with HybridNew
-    #python3 $BASE_DIR/bdt_cut_optimization/runBDTOptimCombine.py -i $COMBINE_DIR -o $EOS_DIR --scan_sensitivity input_combine/sensitivity_tree_bdt_scan_WTau3Mu_$CATEGORY${YEAR}_$TAG.root -d WTau3Mu_$CATEGORY$YEAR -n WTau3Mu_$CATEGORY$YEAR -M HybridNew -s all
+    python3 $BASE_DIR/models/runBDTOptimCombine.py -i $COMBINE_DIR -o $EOS_DIR --scan_sensitivity input_combine/sensitivity_tree_bdt_scan_WTau3Mu_$CATEGORY${YEAR}_$TAG.root -d WTau3Mu_$CATEGORY$YEAR -n WTau3Mu_$CATEGORY$YEAR -M HybridNew --BDTmin 0.9900 --BDTmax 0.9910 --BDTstep 0.0005 -s all
     # compare the methods
-    #python3 $BASE_DIR/models/compareLimitScan.py --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels AsymptoticLimits --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels HybridNew -o $EOS_DIR -d WTau3Mu_$CATEGORY${YEAR} -n WTau3Mu_$CATEGORY${YEAR} -y 20$YEAR
+    python3 $BASE_DIR/models/compareLimitScan.py --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels AsymptoticLimits --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels HybridNew -o $EOS_DIR -d WTau3Mu_$CATEGORY${YEAR} -n WTau3Mu_$CATEGORY${YEAR} -y 20$YEAR
 
     # compare with expo-only bkg fit
-    python3 $BASE_DIR/models/compareLimitScan.py --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels 'AL-const' --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels 'HN-const' --inputs ../binBDT_LxyS1.5_HLT_overlap_2024May27/input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels AL-expo --inputs ../binBDT_LxyS1.5_HLT_overlap_2024May27/input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels HN-expo -o $EOS_DIR -t HNvsAL_expoVSconst -d WTau3Mu_$CATEGORY${YEAR} -n WTau3Mu_$CATEGORY${YEAR} -y 20$YEAR
+    #python3 $BASE_DIR/models/compareLimitScan.py --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels 'AL-const' --inputs input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels 'HN-const' --inputs ../binBDT_LxyS1.5_HLT_overlap_2024May27/input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.AsymptoticLimits.root --labels AL-expo --inputs ../binBDT_LxyS1.5_HLT_overlap_2024May27/input_combine/Tau3MuCombine.WTau3Mu_$CATEGORY${YEAR}_BDTscan.HybridNew.root --labels HN-expo -o $EOS_DIR -t HNvsAL_expoVSconst -d WTau3Mu_$CATEGORY${YEAR} -n WTau3Mu_$CATEGORY${YEAR} -y 20$YEAR
 done
